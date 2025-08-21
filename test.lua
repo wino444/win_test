dd2a05bf998beb8b8543c5a48014cbd1281b7a83
wino444 <146556853+wino444@users.noreply.github.com>
2025-08-22 01:40:54 +0700

    test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,4 @@
-ไม่มีอะไร
+local CollectModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/wino444/wino444_CollectModule/main/.lua"))()
+
+--เก็บปืน ทั้งหมด
+_G.AutoCollectModule.CollectGuns()
